df46c48a2844d8eb1d4b317b6a92f633518d04d6
panda1 <blueblue0011@gmail.com>
2022-07-09 09:10:36 -0700

    fixed while loop counting down with different name variable

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ while(counter < 51) {
     console.log(counter);
     counter = counter +1;
 }
+
+let counter1 = 0;
+
+while (counter1 > -51) {
+    console.log(counter1);
+    counter1 = counter1 -1;
+}
