7d2f6496f444188acc2b129d24bb195038a1c585
panda1 <blueblue0011@gmail.com>
2022-07-09 10:09:13 -0700

    fixed error, spelling mistakes

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ while(counter3 < ball_players.length) {
 
 let player_scores = [100, 77, 88];
 let counter4 = 0;
-while (counter4 < ball_players.lenth) {
-    console.log(ball_players[counter4] + player_scores[counter4]);
+while (counter4 < ball_players.length) {
+    console.log(`${ball_players[counter4]}  ${player_scores[counter4]}`);
     counter4 = counter4 +1;
 }
