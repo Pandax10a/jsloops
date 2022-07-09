fe2fc1351bf523fd8dc0e0869ab2a122cf1c10d2
panda1 <blueblue0011@gmail.com>
2022-07-09 11:01:53 -0700

    added for loop

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ while(counter2 < 101) {
     console.log(counter2);
     counter2 = counter2 +2;
 }
+for(let counter2 = 0; counter2 < 101; counter2 = counter2 +2){
+    console.log(counter2);
+}
 
 let ball_players = [`abe`, `homer`, `bart`];
 let counter3 = 0;
@@ -23,6 +26,10 @@ while(counter3 < ball_players.length) {
     console.log(ball_players[counter3]);
     counter3 = counter3 +1;
 }
+for (let counter3 = 0; counter3 < ball_players.length; counter3++){
+    console.log(ball_players[counter3]);
+}
+
 
 let player_scores = [100, 77, 88];
 let counter4 = 0;
