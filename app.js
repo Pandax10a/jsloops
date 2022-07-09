68b0dc529129b0fe8429a769d86fef56913e2aa2
panda1 <blueblue0011@gmail.com>
2022-07-09 09:13:50 -0700

    fixed infinite loop, used wrong variable

--- app.js
+++ app.js
@@ -10,3 +10,9 @@ while (counter1 > -51) {
     console.log(counter1);
     counter1 = counter1 -1;
 }
+
+let counter2 = 0;
+while(counter2 < 101) {
+    console.log(counter2);
+    counter2 = counter2 +2;
+}
